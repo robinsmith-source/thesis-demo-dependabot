8c4dd5b7e3151c5994d1e9708587177203a09c9d
Robin Schmidt <schmidtrobin02@gmail.com>
2024-01-29 21:41:26 +0100

    fix: update recipe creation test, now works with webkit

--- e2e/recipe-create.spec.ts
+++ e2e/recipe-create.spec.ts
@@ -2,46 +2,48 @@ import { test, expect } from "../playwright/fixtures";
 
 test("Create a recipe and make sure it gets created", async ({ page }) => {
   await page.goto("http://localhost:3000/recipe/create");
+  await page.locator(".group > .relative > .inline-flex").first().click();
   await page.getByPlaceholder("My tasty Pizza").fill("Testing Recipe");
   await page.getByLabel("Easy,").click();
-  await page.getByLabel("Hard", { exact: true }).getByText("Hard").click();
+  await page.getByLabel("Medium", { exact: true }).getByText("Medium").click();
+  await page.getByPlaceholder("My grandma used to make this").click();
   await page
     .getByPlaceholder("My grandma used to make this")
-    .fill("Test Description");
-  await page
-    .locator("div")
-    .filter({ hasText: /^Recipe Tags \(0\/10\)$/ })
-    .locator("div")
-    .click();
+    .fill("Description");
+  await page.getByLabel("Recipe Tags (0/10)").click();
   await page.getByLabel("Recipe Tags (0/10)").fill("tag");
   await page.getByLabel("Recipe Tags (0/10)").press("Enter");
   await page.getByRole("button", { name: "Add Step" }).click();
-  await page.getByLabel("Step Description").fill("Step 1 Description");
-  await page
-    .locator(".grid > div:nth-child(2) > .relative > .inline-flex")
-    .click();
-  await page.getByLabel("Duration (in minutes)").fill("10");
-  await page.getByLabel(",", { exact: true }).click();
-  await page.getByLabel("Prep", { exact: true }).getByText("Prep").click();
+  await page.getByLabel("Step Description").fill("Step1");
   await page
     .getByRole("button", { name: "Add Ingredient", exact: true })
     .click();
-  await page.getByLabel("Name", { exact: true }).fill("Ingredient 1");
-  await page.getByLabel("Quantity").fill("4");
-  await page.getByLabel(",", { exact: true }).click();
-  await page.getByLabel("Milliliter", { exact: true }).click();
+  await page.getByLabel("Name", { exact: true }).fill("Ingredient1");
   await page.getByRole("button", { name: "Submit" }).click();
 
-  // wait for the page to not be a recipe
+  await expect(
+    page.locator("h1").filter({ hasText: "Testing Recipe" }),
+  ).toBeVisible();
+  await expect(
+    page
+      .locator("span")
+      .filter({ hasText: "Testing Recipe" })
+      .locator("path")
+      .nth(1),
+  ).toBeVisible();
+
   await page.waitForURL(/\/recipe\/c[a-z0-9]{24}/);
 
-  //TODO: due to layout changes, this assertions are broken and needs to be fixed
-  await expect(page.getByRole("heading")).toContainText("Testing Recipe");
-  await expect(page.getByRole("main")).toContainText("(hard)");
-  await expect(page.getByRole("listitem")).toContainText("4 ml Ingredient 1");
-  await page.getByText("Step 1 Description").click();
-  await expect(page.getByRole("table")).toContainText("Step 1 Description");
-  await page.getByText("minutes (prep)").click();
-  await expect(page.getByRole("table")).toContainText("10 minutes (prep)");
-  await expect(page.getByRole("main")).toContainText("#tag");
+  await expect(page.getByRole("main")).toContainText("Description");
+  await expect(page.getByRole("rowheader")).toContainText("1 g");
+  await expect(
+    page.getByLabel("Ingredient Table").locator("tbody"),
+  ).toContainText("Ingredient1");
+  await expect(page.getByRole("table")).toContainText("1 g Ingredient1");
+  await expect(
+    page
+      .locator("div")
+      .filter({ hasText: /^#tag$/ })
+      .nth(1),
+  ).toBeVisible();
 });
